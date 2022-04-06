3783dbded59a32a5db58abddb76dab9b78ef9822
Nils Bore <nilsbore@gmail.com>
2022-04-06 21:30:45 +0200

    Reorg with new files

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "creating docker image with Ubuntu version: $UBUNTU and ROS version: $DISTR
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t release-action --build-arg ubuntu="$UBUNTU" distro="$DISTRO" . && docker run release-action
+docker build -t release-action --build-arg ubuntu="$UBUNTU" --build-arg distro="$DISTRO" . && docker run --env GITHUB_WORKSPACE -v ${GITHUB_WORKSPACE}:${GITHUB_WORKSPACE} release-action
