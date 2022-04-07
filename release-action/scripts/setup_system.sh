5054884810175b703a04b6b81aceea3331680dcf
Nils Bore <nilsbore@gmail.com>
2022-04-07 21:55:41 +0200

    Bugfix

--- release-action/scripts/setup_system.sh
+++ release-action/scripts/setup_system.sh
@@ -9,13 +9,13 @@ echo "Installing SMaRC for ROS $1"
 sudo curl https://raw.githubusercontent.com/smarc-project/rosinstall/master/sources.list.d/smarc-${1}-latest.list -o /etc/apt/sources.list.d/smarc-latest.list
 sudo curl https://raw.githubusercontent.com/smarc-project/rosinstall/master/rosdep/50-smarc-${1}.list -o /etc/ros/rosdep/sources.list.d/50-smarc.list
 sudo apt update
-rosdep update
 
 if [ "$1" == "melodic" ]; then
-  sudo apt install -y python-bloom
+  sudo apt install -y python-bloom python-rosdep
 else
-  sudo apt install -y python3-bloom
+  sudo apt install -y python3-bloom python3-rosdep
 fi
+rosdep update
 
 # Install necessary deps for bloom and deb packaging
 sudo apt install -y fakeroot dpkg-dev debhelper zip
