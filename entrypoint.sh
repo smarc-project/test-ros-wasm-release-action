e1175d600fdc6220cca2a3aae61907edbb25966c
Nils Bore <nilsbore@gmail.com>
2022-04-08 21:37:45 +0200

    Bugfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ DISTRO=$2
 
 cd /release-action
 echo "creating docker image with Ubuntu version: $UBUNTU and ROS version: $DISTRO"
-echo "propagating github workspace: $GITHUB_WORKSPACE"
+echo "propagating github workspace: $GITHUB_WORKSPACE with contents:"
+ls $GITHUB_WORKSPACE
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
